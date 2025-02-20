9c0117450f0b64a466828eb2247374568b1b15f4
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-21 00:30:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from bot.telegram_bot import bot
 from website.routes import app
+from database import Database
 db = Database(os.getenv("DATABASE_URL"))
 async def run_bot():
     await bot.start()
