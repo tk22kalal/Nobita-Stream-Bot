8cad9aa0520d2c566b80a28d83fcb6d552847c4d
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-21 00:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os
 import asyncio
 from bot.telegram_bot import bot
 from website.routes import app
 from database import Database
+
 db = Database(os.getenv("DATABASE_URL"))
+
 async def run_bot():
     await bot.start()
     await bot.stop()
 
 async def main():
-    await db.connect()
+    # No need to call db.connect() for MongoDB
     server_task = asyncio.create_task(app.run_task(host='0.0.0.0', port=os.getenv('PORT', 5000)))
     bot_task = asyncio.create_task(run_bot())
     await asyncio.gather(server_task, bot_task)
