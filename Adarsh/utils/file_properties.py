d695962051c7e1f72ba479cc105f38fb21b5f106
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-20 22:20:43 +0530

    Update file_properties.py

--- Adarsh/utils/file_properties.py
+++ Adarsh/utils/file_properties.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from pyrogram.file_id import FileId
 from pyrogram.raw.types.messages import Messages
 from Adarsh.server.exceptions import FIleNotFound
-
+from Adarsh.utils.helpers import get_file_ids  # ✅ Import from helpers.py
 
 async def parse_file_id(message: "Message") -> Optional[FileId]:
     media = get_media_from_message(message)
@@ -15,19 +15,6 @@ async def parse_file_unique_id(message: "Messages") -> Optional[str]:
     media = get_media_from_message(message)
     if media:
         return media.file_unique_id
-
-async def get_file_ids(client: Client, chat_id: int, id: int) -> Optional[FileId]:
-    message = await client.get_messages(chat_id, id)
-    if message.empty:
-        raise FIleNotFound
-    media = get_media_from_message(message)
-    file_unique_id = await parse_file_unique_id(message)
-    file_id = await parse_file_id(message)
-    setattr(file_id, "file_size", getattr(media, "file_size", 0))
-    setattr(file_id, "mime_type", getattr(media, "mime_type", ""))
-    setattr(file_id, "file_name", getattr(media, "file_name", ""))
-    setattr(file_id, "unique_id", file_unique_id)
-    return file_id
 
 def get_media_from_message(message: "Message") -> Any:
     media_types = (
@@ -45,7 +32,6 @@ def get_media_from_message(message: "Message") -> Any:
         if media:
             return media
 
-
 def get_hash(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
     return getattr(media, "file_unique_id", "")[:6]
@@ -53,14 +39,13 @@ def get_hash(media_msg: Message) -> str:
 def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
     file_name = getattr(media, 'file_name', "")
-    
+
     # If file_name is absent, use the caption as a fallback
     if not file_name and media_msg.caption:
         return media_msg.caption.html
     else:
         return file_name
 
-        
 def get_media_file_size(m):
     media = get_media_from_message(m)
     return getattr(media, "file_size", 0)
