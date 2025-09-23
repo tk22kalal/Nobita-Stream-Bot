6378b2eb0fbf98a6c3c0f4fadadd60e6c585cdac
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-09-24 01:10:56 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,11 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
+        
+    @classmethod
+    def reset_batch(cls):
+        # Not needed now, but keeping it in case of legacy code
+        pass
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
