0c33c859416af2be66c5855f8636e8277263dba1
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-09-24 01:04:58 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,10 +1,10 @@
 import os
-import random
 from os import getenv, environ
 from dotenv import load_dotenv
 
-load_dotenv()
 
+
+load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
@@ -15,62 +15,25 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    DB_CHANNEL = int(getenv('DB_CHANNEL', '0'))
-    DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-    PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "True") == "True"
-    CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
     PORT = int(getenv('PORT', 8080))
-    BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split() if x.isdigit())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NobiDeveloperr'))
-
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
+    
     else:
         ON_HEROKU = False
-
-    _FQDN_LIST = [f"stream{i}.nextpulse.workers.dev" for i in range(1, 11)]
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL', True))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split() if x.isdigit()))
-
-    _file_fqdn_map = {}  # file_id -> fqdn
-    _current_fqdn = random.choice(_FQDN_LIST)
-    _current_repeat = 0
-
-    # Default (not important now)
-    FQDN = _current_fqdn
-    URL = f"https://{FQDN}/"
-
-    @classmethod
-    def get_fqdn_for_file(cls, file_id: str) -> str:
-        if file_id not in cls._file_fqdn_map:
-            # If 2 requests already used current, pick a new one
-            if cls._current_repeat >= 2:
-                new_fqdn = random.choice(cls._FQDN_LIST)
-                while new_fqdn == cls._current_fqdn:
-                    new_fqdn = random.choice(cls._FQDN_LIST)
-                cls._current_fqdn = new_fqdn
-                cls._current_repeat = 0
-
-            cls._file_fqdn_map[file_id] = cls._current_fqdn
-            cls._current_repeat += 1
-
-        return cls._file_fqdn_map[file_id]
-
-    @classmethod
-    def get_url_for_file(cls, file_id: str) -> str:
-        return f"https://{cls.get_fqdn_for_file(file_id)}/"
-
-    @classmethod
-    def reset_batch(cls):
-        # Not needed now, but keeping it in case of legacy code
-        pass
-
-
-# Instantiate the config object
-Var = Var()
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
