708ca475bddbf2f8850cfeaf1db44730b3317a68
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-09-23 13:45:00 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -1,44 +1,77 @@
-import jinja2
-import urllib.parse
-import logging
-import aiohttp
-
 from Adarsh.vars import Var
 from Adarsh.bot import StreamBot
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.file_properties import get_file_ids
 from Adarsh.server.exceptions import InvalidHash
+import urllib.parse
+import aiofiles
+import logging
+import aiohttp
 
-async def render_page(id, secure_hash, src=None):
+async def render_generate_page(id, secure_hash):
     file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
+        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
+        logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
+    
+    stream_link = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+    download_link = urllib.parse.urljoin(Var.URL, f'dl/{secure_hash}{str(id)}')
 
-    src = urllib.parse.urljoin(
-        Var.URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
-    )
+    async with aiofiles.open('Adarsh/template/generate.html') as r:
+        html = await r.read()
+        html = html.replace('{{ stream_link }}', stream_link)
+        html = html.replace('{{ download_link }}', download_link)
+        return html
 
-    tag = file_data.mime_type.split("/")[0].strip()
-    file_size = humanbytes(file_data.file_size)
-
-    if tag in ["video", "audio"]:
-        template_file = "Adarsh/template/req.html"
+async def render_page(id, secure_hash):
+    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+    if file_data.unique_id[:6] != secure_hash:
+        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
+        logging.debug(f"Invalid hash for message with - ID {id}")
+        raise InvalidHash
+    src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+    
+    if str(file_data.mime_type.split('/')[0].strip()) == 'video':
+        async with aiofiles.open('Adarsh/template/req.html') as r:
+            heading = 'Watch {}'.format(file_data.file_name)
+            tag = file_data.mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+    elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
+        async with aiofiles.open('Adarsh/template/req.html') as r:
+            heading = 'Listen {}'.format(file_data.file_name)
+            tag = file_data.mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
     else:
-        template_file = "Adarsh/template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get("Content-Length")))
+        async with aiofiles.open('Adarsh/template/dl.html') as r:
+            async with aiohttp.ClientSession() as s:
+                async with s.get(src) as u:
+                    heading = 'Download {}'.format(file_data.file_name)
+                    file_size = humanbytes(int(u.headers.get('Content-Length')))
+                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+    current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}'
+    html_code = f'''
+   <p>
+    <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+    <center>
+        <button style="font-size: 20px; background-color: skyblue; border-radius: 10px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX player</button> &nbsp
+        <button style="font-size: 20px; background-color: orange; border-radius: 10px;" onclick="window.location.href = 'vlc://{current_url}'">VLC player</button> &nbsp <br>
+        <p>&nbsp</p>
+        <button style="font-size: 20px; background-color: red; border-radius: 10px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button> &nbsp <br>
+        <p>&nbsp</p>
+        <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+    </center>
+</p>
+</p>
+<center>
+    <h2>
+        <a href="https://telegram.me/BotszList">
+            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="BotszList" width="150" height="75">
+        </a>
+    </h2>
+</center>
 
-    # Read template using Jinja2
-    with open(template_file) as f:
-        template = jinja2.Template(f.read())
+'''
 
-    return template.render(
-        file_name=file_data.file_name.replace("_", " "),
-        file_url=src,
-        file_size=file_size,
-        tag=tag,
-        file_unique_id=file_data.unique_id
-    )
+    html += html_code    
+    return html
